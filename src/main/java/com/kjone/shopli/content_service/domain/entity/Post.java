c7946bfaf910c5f508627858b274640f090e3317
kjone8371 <kjone8371@gmail.com>
2024-06-12 09:36:33 +0900

    게시물 엔티티 추가

--- src/main/java/com/kjone/shopli/content_service/domain/entity/Post.java
+++ src/main/java/com/kjone/shopli/content_service/domain/entity/Post.java
@@ -1,19 +1,43 @@
 package com.kjone.shopli.content_service.domain.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.kjone.shopli.user_service.domain.user.Profile;
+import com.kjone.shopli.user_service.domain.user.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
-@Table
+@Table(name = "post")
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String title;
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "profile_id", nullable = false)
+    private Profile profile;
 }
