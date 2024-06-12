86be72868b782062ce2c2d2653c9ea3924af9e84
kjone8371 <kjone8371@gmail.com>
2024-06-12 09:28:15 +0900

    프로필 엔티티 수정

--- src/main/java/com/kjone/shopli/user_service/domain/user/Profile.java
+++ src/main/java/com/kjone/shopli/user_service/domain/user/Profile.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "user_profile")
 @Data
 @Builder
 @NoArgsConstructor
@@ -15,19 +16,16 @@ public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nickname;
-
     @JsonIgnore
     @Column(length = 128)
     private Long image; // 이미지
-
     private int phone; // 전화번호
-
-
     private Long my_post; // 내 게시글
 
-
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
 
 }
