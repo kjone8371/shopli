29f7741108de5348d990cc57d4e95fd186a03c73
kjone8371 <kjone8371@gmail.com>
2024-06-12 09:27:48 +0900

    게시물 엔티티 추가

---
+++ src/main/java/com/kjone/shopli/content_service/domain/entity/Post.java
@@ -0,0 +1,19 @@
+package com.kjone.shopli.content_service.domain.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+}
