8bc919e13b1874a19bf4e8e7a4e9218545cdd180
kjone8371 <kjone8371@gmail.com>
2024-06-12 09:38:08 +0900

    게시물 controller 추가

---
+++ src/main/java/com/kjone/shopli/content_service/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.kjone.shopli.content_service.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
+public class PostController {
+
+}
