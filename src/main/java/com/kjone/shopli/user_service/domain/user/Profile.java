5e48de683fe2185f45de43555faa6149382dff5c
kjone8371 <kjone8371@gmail.com>
2024-06-12 10:38:00 +0900

    JsonIgnore 어노테이션 사용

--- src/main/java/com/kjone/shopli/user_service/domain/user/Profile.java
+++ src/main/java/com/kjone/shopli/user_service/domain/user/Profile.java
@@ -38,6 +38,7 @@ public class Profile {
     private LocalDateTime updateTime = LocalDateTime.now();
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
