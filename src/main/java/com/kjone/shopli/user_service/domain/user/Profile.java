33910a51fac19301c97f04773e6a2ad97b481598
kjone8371 <kjone8371@gmail.com>
2024-06-12 09:10:23 +0900

    프로필 엔티티 추가

---
+++ src/main/java/com/kjone/shopli/user_service/domain/user/Profile.java
@@ -0,0 +1,33 @@
+package com.kjone.shopli.user_service.domain.user;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    @JsonIgnore
+    @Column(length = 128)
+    private Long image; // 이미지
+
+    private int phone; // 전화번호
+
+
+    private Long my_post; // 내 게시글
+
+
+
+
+}
