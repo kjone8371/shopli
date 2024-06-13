ba4ca7b3deaabdac27861bbff226c6dbc538690c
kjone8371 <kjone8371@gmail.com>
2024-06-13 20:19:22 +0900

    삭제 서비스 추가

--- src/main/java/com/kjone/shopli/user_service/service/UserService.java
+++ src/main/java/com/kjone/shopli/user_service/service/UserService.java
@@ -17,6 +17,7 @@ public interface UserService {
     public List<User> getAllUsers();
     public User createProfile(Long userId, Profile profile) throws Exception;
     public User updateProfile(Long userId, Profile profileDetails) throws Exception;
+    public void deleteProfile(Long userId, Long profileId) throws Exception;
     public Profile getProfile(Long userId, Long profileId) throws Exception;
-        public Optional<User> getUserById(Long id) ;
+    public Optional<User> getUserById(Long id) ;
 }
