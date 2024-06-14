511aa122b322ca3762ef302daa1681eeff096235
kjone8371 <kjone8371@gmail.com>
2024-06-14 12:19:24 +0900

    카트 아이템 응답 엔티티 추가

---
+++ src/main/java/com/kjone/shopli/content_service/domain/response/CartItemResponse.java
@@ -0,0 +1,25 @@
+package com.kjone.shopli.content_service.domain.response;
+
+
+import com.kjone.shopli.content_service.domain.entity.CartItem;
+import com.kjone.shopli.content_service.domain.entity.Item;
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemResponse {
+    private Long id;
+    private String itemName;
+    private int quantity;
+
+    public static CartItemResponse from(CartItem cartItem) {
+        return new CartItemResponse(
+                cartItem.getId(),
+                cartItem.getItem().getName(), // 예시로 Item에서 이름 가져오기
+                cartItem.getQuantity()
+        );
+    }
+}
