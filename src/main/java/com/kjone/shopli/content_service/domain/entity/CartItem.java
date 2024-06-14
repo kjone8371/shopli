51a339003557256618762f3f04eb4378bea2a2e6
kjone8371 <kjone8371@gmail.com>
2024-06-14 12:19:09 +0900

    카트 아이템 엔티티 수정

--- src/main/java/com/kjone/shopli/content_service/domain/entity/CartItem.java
+++ src/main/java/com/kjone/shopli/content_service/domain/entity/CartItem.java
@@ -1,6 +1,7 @@
 package com.kjone.shopli.content_service.domain.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.kjone.shopli.user_service.domain.user.User;
 import jakarta.persistence.*;
 import lombok.*;
@@ -18,12 +19,16 @@ public class CartItem {
     private Long id;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "item_id", nullable = false)
     private Item item;
 
     private int quantity;
+
+
 }
